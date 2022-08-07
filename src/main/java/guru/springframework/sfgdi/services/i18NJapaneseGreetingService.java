8fdbf7340d795564975ef5d3dc258b4a109a8b1c
Phuong Nguyen <phuongsover2@gmail.com>
2022-08-07 21:08:45 +0700

    Using @Profile with default

--- src/main/java/guru/springframework/sfgdi/services/i18NJapaneseGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/i18NJapaneseGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("JP")
+@Profile({"JP", "default"})
 @Service("i18NService")
 public class i18NJapaneseGreetingService implements GreetingService{
 	@Override
