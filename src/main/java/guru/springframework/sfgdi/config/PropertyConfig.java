8407b87c028bdea02a59198dd5324bd95c7f561c
Phuong Nguyen <phuongsover2@gmail.com>
2022-08-10 13:23:02 +0700

    Using Spring Boot application.properties instead of external properties files

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -5,18 +5,8 @@ import guru.springframework.sfgdi.examplebeans.FakeJMSBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources(
-		{
-				@PropertySource("classpath:datasource.properties"),
-				@PropertySource("classpath:jms.properties")
-		}
-)
 public class PropertyConfig {
 	@Value("${guru.username}")
 	String username;
@@ -36,12 +26,6 @@ public class PropertyConfig {
 	@Value("${guru.jms.dbUrl}")
 	String jmsDbUrl;
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
-				new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
 
 	@Bean
 	public FakeDataSource fakeDataSource() {
